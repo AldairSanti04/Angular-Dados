ee850d98dd36dec8e738ee83b7cecabbf453de64
AldairSanti04 <aldairsanti04@gmail.com>
2021-03-06 16:34:55 -0600

    Add function tirarDados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  dado1 = "../assets/img/dice1.png";
+  dado2 = "../assets/img/dice2.png";
+  numero1: number = 0;
+  numero2: number = 0;
 
-
+  tirarDados(): void{
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dado1 =  "../assets/img/dice" + this.numero1 + ".png";
+    this.dado2 =  "../assets/img/dice" + this.numero2 + ".png";
+  }
 }
